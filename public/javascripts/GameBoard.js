f91113711a99eab05677022a0a2ca763b5c5e33b
Rozemarijn Veelers <marijnveelers96@gmail.com>
2018-12-03 13:39:36 +0100

    Mouseover

--- public/javascripts/GameBoard.js
+++ public/javascripts/GameBoard.js
@@ -6,11 +6,11 @@ var gameStateModule = (function () {
 function initTables(){
     let friendlyFleet = document.getElementById("friendly-fleet");
     let enemyFleet = document.getElementById("enemy-fleet");
-    var Table = "<table>"  
+    var Table = "<table width='400'>"  
     for(x=0; x<10;x++){
         Table = Table + "<tr>";
         for(y=0; y<10;y++){
-            Table = Table + "<td> <div x="+x+" y="+y+">" + "(" + x + "," + y + ")" + "</div></td>";
+            Table = Table + "<td height='38'> <div x="+x+" y="+y+">" + "(" + x + "," + y + ")" + "</div></td>";
         }
         Table = Table + "</tr>";  
     }
@@ -19,6 +19,16 @@ function initTables(){
     enemyFleet.innerHTML = Table;
 }
 
+function printcoor(e){
+    x = e.x;
+    y = e.y;
+    coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("friendly-fleet").innerHTML = coor
+}
+
+function clearCoor(){
+    document.getElementById("friendly-fleet").innerHTML = "";
+}
 
 
 
