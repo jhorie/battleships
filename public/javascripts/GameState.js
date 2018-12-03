19b8882923ac31fbc4996657a19ceba7a8e28a66
Jhorie Slot <j.m.a.slot@student.tudelft.nl>
2018-12-03 15:28:17 +0100

    move ship

--- public/javascripts/GameState.js
+++
@@ -1,3 +0,0 @@
-let friendlyFleet = document.getElementById("friendly-fleet");
-
-friendlyFleet.innerHTML = "<table><tr><td>asd</td></tr><tr><td>kasjdalks</td></tr></table>";
--- public/javascripts/Ship.js
+++ public/javascripts/Ship.js
@@ -1,9 +1,38 @@
-var shipModule = (function (length, coordinate, direction) {
+var shipModule = (function (length, coordinate, direction, id) {
     let lengthShip = length;
     let coordinateShip = coordinate;
     let directionShip = direction;
     let isPartOfShipBombed = []; //// [0] -> coordinateShip
-    let divElement;
+    let movingState = false;
+    let movingX = 0;
+    let movingY = 0;
+
+
+    let divElement = document.createElement("div");
+    divElement.className = "ship-" + length;
+    divElement.id = "ship-" + id;
+    divElement.onmousedown = function (event) {
+        movingState = true;
+        movingX = event.x;
+        movingY = event.y;
+    };
+    divElement.onmouseup = function (event) {
+        movingState = false;
+    }
+    divElement.onmousemove = function (event) {
+        if (movingState) {
+            console.log(divElement.style.left);
+            console.log(divElement.style.top);
+
+            divElement.style.left = (divElement.getBoundingClientRect().left - (movingX - event.x)) + 'px';
+            divElement.style.top = (divElement.getBoundingClientRect().top - (movingY - event.y)) + 'px';
+            movingX = event.x;
+            movingY = event.y;
+        }
+    }
+
+    divElement.innerHTML = "<img src=\"images/plain-triangle.png\" draggable=\"false\" class=\"img-ship\">";
+
 
     for (let i = 0; i < lengthShip; i++) {
         isPartOfShipBombed.push(false)
@@ -34,26 +63,23 @@ var shipModule = (function (length, coordinate, direction) {
             console.log("SPLASH");
             return false;
         },
+        getDivElement: function () {
+            return divElement;
+        },
     }
 });
-
+var ship1 = new shipModule(2, null, Direction.South, 1);
+var ship2 = new shipModule(3, null, Direction.South, 2);
+var ship3 = new shipModule(3, null, Direction.South, 3);
+var ship4 = new shipModule(4, null, Direction.South, 4);
+var ship5 = new shipModule(5, null, Direction.South, 5);
 function initShips() {
 
     let catalogue = document.getElementById("catalogue");
 
+    catalogue.appendChild(ship1.getDivElement());
 
-    catalogue.innerHTML += getShipHTML(2, 1);
-    catalogue.innerHTML += getShipHTML(3, 2);
-    catalogue.innerHTML += getShipHTML(3, 3);
-    catalogue.innerHTML += getShipHTML(4, 4);
-    catalogue.innerHTML += getShipHTML(5, 5);
 
 
     console.log("One ship added");
-}
-
-function getShipHTML(shipLength, id) {
-    return "<div class=\"ship-" + shipLength + "\" id='ship-" + id + "'>" +
-        "<img src=\"images/plain-triangle.png\" class=\"img-ship\">" +
-        "</div>"
 }
