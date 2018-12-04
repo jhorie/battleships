b05aae89c508b69f2400792d0f2d0e7542ee6687
Rozemarijn Veelers <marijnveelers96@gmail.com>
2018-12-04 15:29:36 +0100

    Niet gebruiken

--- public/javascripts/GameBoard.js
+++ public/javascripts/GameBoard.js
@@ -6,11 +6,11 @@ var gameStateModule = (function () {
 function initTables(){
     let friendlyFleet = document.getElementById("friendly-fleet");
     let enemyFleet = document.getElementById("enemy-fleet");
-    var Table = "<table width='400'>"  
+    var Table = "<table width=400'>"  
     for(x=0; x<10;x++){
         Table = Table + "<tr>";
         for(y=0; y<10;y++){
-            Table = Table + "<td height='38'> <div x="+x+" y="+y+">" + "(" + x + "," + y + ")" + "</div></td>";
+            Table = Table + "<td height='40'> <div x="+x+" y="+y+">" + "(" + x + "," + y + ")" + "</div></td>";
         }
         Table = Table + "</tr>";  
     }
