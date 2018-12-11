0d515be29d709db7f3587740a76e24dd74f21f50
Jhorie Slot <j.m.a.slot@student.tudelft.nl>
2018-12-11 14:12:40 +0100

    update

--- public/javascripts/Direction.js
+++ public/javascripts/Direction.js
@@ -1,1 +1,7 @@
-const Direction = {"East": 1, "South": 0};
+var directionModule = (function () {
+    return {
+        Direction: {"East": 1, "South": 0}
+    }
+})();
+
+module.exports = directionModule;
